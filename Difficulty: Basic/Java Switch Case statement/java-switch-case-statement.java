89c2a0e2403e3cfee06f26280333c0b325aa9e03
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2026-06-30 21:48:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Switch Case statement/java-switch-case-statement.java
@@ -0,0 +1,18 @@
+class Solution {
+    static double switchCase(int choice, List<Double> arr) {
+        // code here
+        switch(choice){
+            case 1:
+                double radius = arr.get(0);
+                return Math.PI*radius*radius;
+                
+            case 2:
+                double length = arr.get(0);
+                double breadth = arr.get(1);
+                return length * breadth;
+                
+            default:
+                return 0.0;
+        }
+    }
+}

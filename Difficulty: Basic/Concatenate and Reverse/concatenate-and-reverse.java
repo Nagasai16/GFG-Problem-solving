8ae48a2177250638c04b6de253d0fb8dbf75f236
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2026-08-15 22:50:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate and Reverse/concatenate-and-reverse.java
@@ -0,0 +1,8 @@
+class Solution {
+    static String conRevstr(String s1, String s2) {
+        // code here
+        return new StringBuilder(s1+s2).reverse().toString();
+        
+        
+    }
+}

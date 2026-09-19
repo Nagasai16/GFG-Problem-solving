57b08ecbbb7ba889dd8f9b4b84a3b0d0ad89fd51
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2026-09-19 22:26:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of GP/sum-of-gp.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sumOfGP(int n, int a, int r) {
+        // code here
+        int sum = 0;
+        int term = a;
+        for(int i=0;i<n;i++){
+            sum += term;
+            term *= r;
+        }
+        return sum;
+    }
+}

011b8d37fcaa85b8fde108060ccb92dcc43cb062
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2026-09-20 18:49:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check Power of 3/check-power-of-3.java
@@ -0,0 +1,10 @@
+class Solution {
+    static boolean isPowerOf3(int n) {
+        // code here
+        while(n>1 && n%3==0){
+            n /= 3;
+        }
+        return n==1;
+        
+    }
+}
